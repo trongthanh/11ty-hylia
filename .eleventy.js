13d0aa56290427ec85e449bbd54ed0af09096682
Thanh Tran <trongthanh@gmail.com>
2020-05-20 22:49:50 +0700

    Migrate indent character from space to tab

--- .eleventy.js
+++ .eleventy.js
@@ -15,63 +15,63 @@ const parseTransform = require('./_11ty/transforms/parse-transform.js');
 const site = require('./_data/site.json');
 
 module.exports = function(config) {
-  // Filters
-  config.addFilter('dateFilter', dateFilter);
-  config.addFilter('markdownFilter', markdownFilter);
-  config.addFilter('w3DateFilter', w3DateFilter);
+	// Filters
+	config.addFilter('dateFilter', dateFilter);
+	config.addFilter('markdownFilter', markdownFilter);
+	config.addFilter('w3DateFilter', w3DateFilter);
 
-  // Layout aliases
-  config.addLayoutAlias('home', 'layouts/home.njk');
+	// Layout aliases
+	config.addLayoutAlias('home', 'layouts/home.njk');
 
-  // Transforms
-  config.addTransform('htmlmin', htmlMinTransform);
-  config.addTransform('parse', parseTransform);
+	// Transforms
+	config.addTransform('htmlmin', htmlMinTransform);
+	config.addTransform('parse', parseTransform);
 
-  // Passthrough copy
-  config.addPassthroughCopy('./fonts');
-  config.addPassthroughCopy('./images');
-  config.addPassthroughCopy('./js');
-  config.addPassthroughCopy('node_modules/nunjucks/browser/nunjucks-slim.js');
-  config.addPassthroughCopy('./robots.txt');
+	// Passthrough copy
+	config.addPassthroughCopy('./fonts');
+	config.addPassthroughCopy('./images');
+	config.addPassthroughCopy('./js');
+	config.addPassthroughCopy('node_modules/nunjucks/browser/nunjucks-slim.js');
+	config.addPassthroughCopy('./robots.txt');
 
-  const now = new Date();
+	const now = new Date();
 
-  // Custom collections
-  const livePosts = (post) => post.date <= now && !post.data.draft;
-  config.addCollection('posts', (collection) => {
-    return [...collection.getFilteredByGlob('./posts/*.md').filter(livePosts)].reverse();
-  });
+	// Custom collections
+	const livePosts = (post) => post.date <= now && !post.data.draft;
+	config.addCollection('posts', (collection) => {
+		return [...collection.getFilteredByGlob('./posts/*.md').filter(livePosts)].reverse();
+	});
 
-  config.addCollection('postFeed', (collection) => {
-    return [...collection.getFilteredByGlob('./posts/*.md').filter(livePosts)]
-      .reverse()
-      .slice(0, site.maxPostsPerPage);
-  });
+	config.addCollection('postFeed', (collection) => {
+		return [...collection.getFilteredByGlob('./posts/*.md').filter(livePosts)]
+			.reverse()
+			.slice(0, site.maxPostsPerPage);
+	});
 
-  // Plugins
-  config.addPlugin(rssPlugin);
-  config.addPlugin(syntaxHighlight);
+	// Plugins
+	config.addPlugin(rssPlugin);
+	config.addPlugin(syntaxHighlight);
 
-  // 404
-  config.setBrowserSyncConfig({
-    callbacks: {
-      ready: function(err, browserSync) {
-        const content_404 = fs.readFileSync('dist/404.html');
+	// 404
+	config.setBrowserSyncConfig({
+		callbacks: {
+			ready: function(err, browserSync) {
+				const content_404 = fs.readFileSync('_site/404.html');
 
-        browserSync.addMiddleware('*', (req, res) => {
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          res.end();
-        });
-      },
-    },
-  });
+				browserSync.addMiddleware('*', (req, res) => {
+					// Provides the 404 content without redirect.
+					res.write(content_404);
+					res.end();
+				});
+			},
+		},
+	});
 
-  return {
-    dir: {
-      input: '.',
-      output: '_site',
-    },
-    passthroughFileCopy: true,
-  };
+	return {
+		dir: {
+			input: '.',
+			output: '_site',
+		},
+		passthroughFileCopy: true,
+	};
 };
